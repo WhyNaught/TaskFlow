61c5fc45e88731b5ef67bfd4eb2e51a3d1e17bd7
Umar <umardxbfarooq@gmail.com>
2024-10-25 00:16:44 -0400

    Initialized frontend

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
-const port = 3000; 
 const router = require('../server/routes/authrouter');
+let port; 
 
+// setting up ports 
+if (process.env.status === 'production') {
+    port = process.env.PROD_PORT; 
+} else if (process.env.status === 'development') {
+    port = process.env.DEV_PORT;
+}
+
+// starting app
 app.listen(port, 'localhost', () => {
     console.log('Started on port ' + port); 
 })
 
+// linking app to express router
 app.use(router);
--- server/routes/authrouter.js
+++ server/routes/authrouter.js
@@ -1,12 +1,16 @@
+// file that contains all necesssary HTTP requests for user authentication
 const express = require('express');
 const router = express.Router();
 
+// TaskFlow homepage
 router.get('/', async (req, res) => {
     res.send('Homepage');
 })
 
-router.post('/register', async (req, res) => {
-    
+// Get request for registation homepage
+router.get('/register', async (req, res) => {
+    res.send('Registration');
 })
 
+// Export to other files
 module.exports = router; 
